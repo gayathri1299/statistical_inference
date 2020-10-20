68e5556bfc6a3227f295ec215e268bad535b3485
gayathri1299 <prerepag@gmail.com>
2020-10-20 15:27:12 +0530

    CODE

--- statistical_inference.R
+++ statistical_inference.R
@@ -8,5 +8,19 @@ qplot(mean_sim, geom = "histogram",
       main = "Mean Simulation histogram", 
       xlab = "Mean observations")  
 
+mean_2 <- mean(mean_sim)
+print(mean_2)
 
+T_mean <- 1/0.2
+print(T_mean)
 
+Eden <- (1/.2)/sqrt(m)
+E_var <- Eden^2
+
+Ssd <- sd(mean_sim)
+S_var <- var(mean_sim)
+
+fit <- seq(min(mean_sim), max(mean_sim), length=100)
+standard_fit <- dnorm(fit, mean=T_mean, sd=Eden)
+hist(mean_sim, breaks = m, prob=T, xlab = "means", ylab = "count", main = "Density of Means")
+lines(fit, standard_fit, pch=2, col="blue", lty=5)
